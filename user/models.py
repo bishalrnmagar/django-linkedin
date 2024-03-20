0b8294ab60626610f10a245a19ca741780e0f433
bishalrnmagar <bishalmagr70@gmail.com>
2024-03-20 08:14:46 +0545

    custom user

--- user/models.py
+++ user/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    profile_img = models.URLField(default="https://lh3.googleusercontent.com/a/ACg8ocIcAiQQxIElxmMp_3z5RAmSwyJGuK_mKmFu1JT0rFRANLQ=s360-c-no")    
+
+    def __str__(self):
+        return self.get_full_name()
